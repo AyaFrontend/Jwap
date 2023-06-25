c63e56f4d8d1a6c24d79fb7da28f9dd30a3d018e
AyaFrontend <ayamohamedabdelrahman868@gmail.com>
2023-06-25 12:01:25 -0700

    commit all

---
+++ Jwap/Dtos/CallOfferDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jwap.API.Dtos
+{
+    public class CallOfferDto
+    {
+        public string CallerId { set; get; }
+        public bool StartCall { set; get; }
+        public bool CallState { set; get; }
+        public string CalleeId { set; get; }
+        public string CallType{ set; get; }
+        public string PeerId { set; get; }
+    }
+}
---
+++ Jwap/Dtos/UserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jwap.API.Dtos
+{
+    public class UserDto
+    {
+        public string Id { set; get; }
+        public string Email { set; get; }
+        public string DisplayName { set; get; }
+        public string Token { set; get; }
+    }
+}
